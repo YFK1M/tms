b471e2de9528a2dd1acb2c072b06e2279f787f33
Maksim Spiridonov <laystimer@gmail.com>
2026-01-29 22:47:10 +0300

    feat(auth): Add Login, Register, Refresh. Configure prisma

---
+++ apps/backend/src/auth/auth.controller.ts
@@ -0,0 +1,54 @@
+import {
+    Controller,
+    Post,
+    Body,
+    Res,
+    Req,
+} from '@nestjs/common';
+import type { Response, Request } from 'express';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly auth: AuthService) {}
+
+    @Post('register')
+    async register(
+        @Body() dto: RegisterDto,
+        @Res({ passthrough: true }) res: Response,
+    ) {
+        console.log(dto)
+        const result = await this.auth.register(dto.email, dto.password);
+        if (!result?.id) return null;
+        this.setRefreshCookie(res, result.id);
+        return result;
+    }
+
+    @Post('login')
+    async login(
+        @Body() dto: LoginDto,
+        @Res({ passthrough: true }) res: Response,
+    ) {
+        const result = await this.auth.login(dto.email, dto.password);
+        if (!result?.id) return null;
+        this.setRefreshCookie(res, result.id);
+        return result;
+    }
+
+    @Post('refresh')
+    async refresh(@Req() req: Request) {
+        const userId = req.cookies?.refreshToken;
+        return this.auth.refresh(userId);
+    }
+
+    private setRefreshCookie(res: Response, userId: string) {
+        res.cookie('refreshToken', userId, {
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: false,
+            path: '/auth/refresh',
+        });
+    }
+}
---
+++ apps/backend/src/auth/auth.service.ts
@@ -0,0 +1,74 @@
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthResponse } from '@tms/shared-types/src/generated/types.gen';
+import type { StringValue } from "ms";
+
+interface JwtPayload {
+    sub: string;
+}
+
+const JWT_ACCESS_EXPIRES = (process.env.JWT_ACCESS_EXPIRES ?? '15m') as StringValue;
+
+@Injectable()
+export class AuthService {
+    private readonly accessTokenOptions: JwtSignOptions = {
+        expiresIn: JWT_ACCESS_EXPIRES,
+    };
+
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly jwt: JwtService,
+    ) {}
+
+    async register(email: string, password: string): Promise<AuthResponse> {
+        const exists = await this.prisma.user.findUnique({ where: { email } });
+        if (exists) {
+            throw new ConflictException('User already exists');
+        }
+
+        const passwordHash = await bcrypt.hash(password, 6);
+        console.log(email, passwordHash)
+        const user = await this.prisma.user.create({
+            data: { email, passwordHash },
+        });
+
+        return this.buildAuthResponse(user);
+    }
+
+    async login(email: string, password: string): Promise<AuthResponse> {
+        const user = await this.prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const valid = await bcrypt.compare(password, user.passwordHash);
+        if (!valid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        return this.buildAuthResponse(user);
+    }
+
+    async refresh(userId: string) {
+        return this.buildAccessToken({ sub: userId });
+    }
+
+    private buildAuthResponse(user): AuthResponse {
+        const payload: JwtPayload = { sub: user.id };
+
+        return {
+            id: user.id,
+            email: user.email,
+            createdAt: user.createdAt.toISOString(),
+            accessToken: this.jwt.sign(payload, this.accessTokenOptions),
+        };
+    }
+
+    private buildAccessToken(payload: JwtPayload) {
+        return {
+            accessToken: this.jwt.sign(payload, this.accessTokenOptions),
+        };
+    }
+}
